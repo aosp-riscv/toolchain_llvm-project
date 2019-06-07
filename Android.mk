b953a88be5f5ec0b364c18c72f84321260e50efb
Nick Desaulniers <ndesaulniers@google.com>
2019-06-07 11:55:03 -0700

    [Android] add toplevel Android.mk to stop breakage
    Summary:
    Android is currently moving over to building AOSP's LLVM release from
    the monorepo. In testing, we were hitting breakage due to the Android.mk
    file under pstl/ (parallel STL). Previously, Android was not including
    pstl/ as a separate repo and now with the monorepo we get it no matter
    what (and will probably use it soon).
    
    The issue for Android's build system is that it recursively looks for
    Android.mk files, so it picks up the one under pstl/ even though we
    don't want to build it just yet.
    
    The sugguested fix from Android's build team is to add an empty
    Android.mk to the top level directory, which will prevent the build
    system from recursing further, finding the one in pstl/, and erroring
    out.
    
    Link: https://android-review.googlesource.com/c/platform/manifest/+/977993
    
    Reviewers: srhines, danalbert, MikeDvorskiy
    
    Subscribers: llvm-commits, pirama
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D63017
    
    Change-Id: I6d42ff21a6516bc73acb9a16094bd9e9e495e562

---
+++ Android.mk
@@ -0,0 +1,10 @@
+#===-- Android.mk --------------------------------------------------------===##
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===----------------------------------------------------------------------===##
+#
+# Intentionally empty to prevent Android's build system from loading deeper
+# Android.mk files such as from pstl.
