ccef8eade7c7718dbcff5139b0f8c8919f06ca42
Fangrui Song <maskray@google.com>
2019-08-13 09:43:40 +0000

    [ELF] Don't special case symbolic relocations with 0 addend to ifunc in writable locations
    Currently the following 3 relocation types do not trigger the creation
    of a canonical PLT (which changes STT_GNU_IFUNC to STT_FUNC and
    redirects all references):
    
    1) GOT-generating (`needsGot`)
    2) PLT-generating (`needsPlt`)
    3) R_ABS with 0 addend in a writable location. This is used for
      for ifunc function pointers in writable sections such as .data and .toc.
    
    This patch deletes case 3) to simplify the R_*_IRELATIVE generating
    logic added in D57371. Other advantages:
    
    * It is guaranteed no more than 1 R_*_IRELATIVE is created for an ifunc.
    * PPC64: no need to special case ifunc in toc-indirect to toc-relative relaxation. See D65755
    
    The deleted elf::addIRelativeRelocs demonstrates that one-pass scan
    through relocations makes several optimizations difficult. This is
    something we can think about in the future.
    
    Reviewed By: peter.smith
    
    Differential Revision: https://reviews.llvm.org/D65995
    
    Change-Id: Ia6355f4eff0617eecef0452ca7a65c2fbd1a7222
    llvm-svn: 368661
    (cherry picked from commit dc06b0bc9ad055d06535462d91bfc2a744b2f589)

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -1759,8 +1759,6 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
     reportUndefinedSymbols<ELFT>();
   }
 
-  addIRelativeRelocs();
-
   if (in.plt && in.plt->isNeeded())
     in.plt->addSymbols();
   if (in.iplt && in.iplt->isNeeded())
