3414f1cd7c1d8a8b50d837f87a45de681a23fb25
Fangrui Song <maskray@google.com>
2019-12-14 15:09:43 -0800

    [BACKPORT][ELF] Delete relOff from TargetInfo::writePLT
    This change only affects EM_386. relOff can be computed from `index`
    easily, so it is unnecessarily passed as a parameter.
    
    Both in.plt and in.iplt entries are written by writePLT. For in.iplt,
    the instruction `push reloc_offset` will change because `index` is now
    different. Fortunately, this does not matter because `push; jmp` is only
    used by PLT. IPLT does not need the code sequence.
    
    Reviewed By: grimar, ruiu
    
    Differential Revision: https://reviews.llvm.org/D71518
    [nd: small conflict in lld/ELF/Arch/Mips.cpp due to no cherry-picking
    commit 3837f4273fcc ("[Coding style change] Rename variables so that
    they start with a lowercase letter")]
    (cherry-picked from commit 90d195d026f5c90e26f09614e23d4112b3dfaaa1)
    Bug: 145916209
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    
    Change-Id: Ic5292cf31ed1518ddc7c455e2780d160f4cb2718

--- lld/ELF/Arch/Mips.cpp
+++ lld/ELF/Arch/Mips.cpp
@@ -35,7 +35,7 @@ public:
   void writeGotPlt(uint8_t *buf, const Symbol &s) const override;
   void writePltHeader(uint8_t *buf) const override;
   void writePlt(uint8_t *buf, uint64_t gotPltEntryAddr, uint64_t pltEntryAddr,
-                int32_t index, unsigned relOff) const override;
+                int32_t index) const override;
   bool needsThunk(RelExpr expr, RelType type, const InputFile *file,
                   uint64_t branchAddr, const Symbol &s) const override;
   void relocateOne(uint8_t *loc, RelType type, uint64_t val) const override;
@@ -305,8 +305,7 @@ template <class ELFT> void MIPS<ELFT>::writePltHeader(uint8_t *buf) const {
 
 template <class ELFT>
 void MIPS<ELFT>::writePlt(uint8_t *buf, uint64_t gotPltEntryAddr,
-                          uint64_t pltEntryAddr, int32_t index,
-                          unsigned relOff) const {
+                          uint64_t pltEntryAddr, int32_t /*index*/) const {
   const endianness e = ELFT::TargetEndianness;
   if (isMicroMips()) {
     // Overwrite trap instructions written by Writer::writeTrapInstr.
