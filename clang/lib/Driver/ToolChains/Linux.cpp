bb1ffed7921596bf17ee51f1948376a1b7c8b2e5
luxufan <932494295@qq.com>
2021-11-05 23:17:05 +0800

    add riscv64 multiarch name when library is android

--- clang/lib/Driver/ToolChains/Linux.cpp
+++ clang/lib/Driver/ToolChains/Linux.cpp
@@ -124,6 +124,10 @@ std::string Linux::getMultiarchTriple(const Driver &D,
     return "powerpc64-linux-gnu";
   case llvm::Triple::ppc64le:
     return "powerpc64le-linux-gnu";
+  case llvm::Triple::riscv64:
+    if (IsAndroid)
+      return "riscv64-linux-android";
+    break;
   case llvm::Triple::sparc:
     return "sparc-linux-gnu";
   case llvm::Triple::sparcv9:
